99ab2054ddea915417f730d7bccf86b284f66eb7
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-23 13:14:44 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double Pow(int a,int b)
+{
+    double power = 1;
+    for (int i = 1; i <= Math.Abs(b); i++)
+    power *= a;
+    if (b > 0)
+        return power;
+    else
+        return 1/power;
+}
+
+int a = 3,b = 5;
+Console.WriteLine(b + "^" + a + " = " + Pow(a,b));
+
+a = 2; b = 4;
+Console.WriteLine(b + "^" + a + " = " + Pow(a,b));
